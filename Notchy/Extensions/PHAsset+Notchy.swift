413d3cfd02fb76ce97aafe8132f07db69512b14b
Dan Loewenherz <dan@lionheartsw.com>
2017-11-19 16:05:19 -0600

    make self unowned

--- Notchy/Extensions/PHAsset+Notchy.swift
+++ Notchy/Extensions/PHAsset+Notchy.swift
@@ -9,9 +9,35 @@
 import UIKit
 import Photos
 
+extension PHImageManager {
+    func image(asset: PHAsset, maskType: MaskType, _ completion: @escaping MaskCallback) {
+        //            [1125, 2436]
+        let size = CGSize(width: 1125, height: 2436)
+        let options = PHImageRequestOptions()
+        options.version = .current
+        options.deliveryMode = .highQualityFormat
+        options.resizeMode = .none
+        options.isNetworkAccessAllowed = true
+
+        requestImage(for: asset, targetSize: size, contentMode: .aspectFit, options: options) { image, other in
+            guard let image = image,
+                let other = other else {
+                    completion(nil)
+                    return
+            }
+
+            if let degraded = other[PHImageResultIsDegradedKey] as? String {
+                print(degraded)
+            }
+
+            maskType.applyMask(input: image, watermark: true, completion: completion)
+        }
+    }
+}
+
 extension PHAsset {
     func image(maskType: MaskType, _ completion: @escaping MaskCallback) {
-        DispatchQueue.global(qos: .default).async {
+        DispatchQueue.global(qos: .default).async { [unowned self] in
 //            [1125, 2436]
             let size = CGSize(width: 1125, height: 2436)
             let options = PHImageRequestOptions()
